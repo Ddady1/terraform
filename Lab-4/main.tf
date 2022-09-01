9051e766d6cfcea71a5fbbf7246fa647aa494f71
Dady <ddady1@gmail.com>
2022-09-01 23:14:31 +0300

    google

--- Lab-4/main.tf
+++ Lab-4/main.tf
@@ -1,7 +1,7 @@
 #----------------------------------------------------------
 #  Terraform - From Zero to Certified Professional
 #
-# Build WebServer during Bootstrap with external template  file
+# Build WebServer during Bootstrap with External TEMPLATE File
 #
 # Made by Denis Astahov
 #----------------------------------------------------------
@@ -11,54 +11,51 @@ provider "aws" {
 }
 
 resource "aws_instance" "web" {
-  ami = "ami-09e2d756e7d78558d" // Amazon Linux2
-  instance_type = "t3.micro"
+  ami                    = "ami-09e2d756e7d78558d" // Amazon Linux2
+  instance_type          = "t3.micro"
   vpc_security_group_ids = [aws_security_group.web.id]
-  user_data = templatefile("user_data.sh.tpl", {
-    f_name = "Denis"
-    l_name = "Astahov"
+  user_data = templatefile("user_data.sh.tpl", { // Template File
+    f_name = "David"
+    l_name = "Racha"
     names  = ["John", "Angel", "David", "Victor", "Frank", "Melissa", "Kitana"]
   })
+
   tags = {
-    Name = "WebServer Built by Terraform with external file"
+    Name  = "WebServer Built by Terraform"
     Owner = "David Racha"
   }
 }
 
 resource "aws_security_group" "web" {
-  name = "WebServer-SG"
-  description = "Security Group for WebServer"
+  name        = "WebServer-SG-15"
+  description = "Security Group for my WebServer"
 
   ingress {
     description = "Allow port HTTP"
-    from_port = 80
-    protocol = "tcp"
-    to_port = 80
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   ingress {
     description = "Allow port HTTPS"
-    from_port = 443
-    protocol = "tcp"
-    to_port = 443
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  ingress {
-    description = "Allow port SSH"
-    from_port = 22
-    protocol = "tcp"
-    to_port = 22
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+
   egress {
-    description = "Allow in ALL ports"
-    from_port = 0
-    protocol = "-1"
-    to_port = 0
+    description = "Allow ALL ports"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   tags = {
-    Name = "WebServer SG Built by Terraform"
+    Name  = "WebServer SG by Terraform"
     Owner = "David Racha"
   }
 }
--- Lab-4/user_data.sh.tpl
+++ Lab-4/user_data.sh.tpl
@@ -3,6 +3,7 @@ yum -y update
 yum -y install httpd
 MYIP=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
 
+
 cat <<EOF > /var/www/html/index.html
 <html>
 <h2>Built by Power of <font color="red">Terraform</font></h2><br>
